48d21d916cdcb342b045fbd7e75eae209a95f20f
GabrielSilva88 <gabrielmg.silva@outlook.com>
2021-09-23 17:41:25 -0300

    Criação View/Profile

--- DotEco-APP/src/app/app.module.ts
+++ DotEco-APP/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TitleComponent } from './Shared/title/title.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileComponent } from './Pages/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     NavComponent,
     TitleComponent,
     RegisterComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
