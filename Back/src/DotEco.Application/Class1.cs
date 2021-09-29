9a06b46b795df35521162c56194b96d928b6e3e9
Pedrolustosa <pedroeternalss@gmail.com>
2021-09-29 10:44:31 -0300

    Organização de Projeto - P2(Soluções, projetos e referenciandos)

---
+++ Back/src/DotEco.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DotEco.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Back/src/DotEco.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DotEco.Domain
+{
+    public class Class1
+    {
+    }
+}
