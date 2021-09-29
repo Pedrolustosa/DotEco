75b978014cc380887075117d62af4958e604ef37
GabrielSilva88 <gabrielmg.silva@outlook.com>
2021-09-28 23:08:22 -0300

    Criação e Alteração dos layout View/RequestRecycle

--- DotEco-APP/src/app/app-routing.module.ts
+++ DotEco-APP/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './Pages/login/login.component';
 import { RecoveryComponent } from './Pages/recovery/recovery.component';
 import { ProfileComponent } from './Pages/profile/profile.component';
 import { RegisterComponent } from './Pages/register/register.component';
+import { RequestRecycleComponent } from './Pages/request-recycle/request-recycle.component';
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
   { path: "login/register/login", component: LoginComponent },
   { path: "login/register", component: RegisterComponent },
   { path: "login/recovery", component: RecoveryComponent },
-  { path: "profile", component: ProfileComponent }
+  { path: "profile", component: ProfileComponent },
+  { path: "requestrecycle", component: RequestRecycleComponent}
 ];
 
 @NgModule({
