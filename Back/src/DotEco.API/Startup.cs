03bcded5dbf9e6cd5795f58a3850b2dff05e2555
Pedrolustosa <pedroeternalss@gmail.com>
2021-12-03 00:49:23 -0300

    SQLServerAzure

--- Back/src/DotEco.API/Startup.cs
+++ Back/src/DotEco.API/Startup.cs
@@ -32,7 +32,7 @@ namespace DotEco.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DotEcoContext>(
-                context => context.UseSqlServer(Configuration.GetConnectionString("SQLServer"))
+                context => context.UseSqlServer(Configuration.GetConnectionString("SQLServerAzure"))
             );
 
             services.AddIdentityCore<User>(options =>
