98d71246c3dbe06ea089c2048928894d4cd6d52e
Pedrolustosa <pedroeternalss@gmail.com>
2021-12-05 19:41:12 -0300

    Correção de bug em CollectionData.

---
+++ Back/src/DotEco.Persistence/Migrations/20211205223502_initial_V5.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotEco.Persistence.Migrations
+{
+    public partial class initial_V5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
